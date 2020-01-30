1f7d6330a5d0af1d74e40a08b1fc2b95b6f08194
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-01-29 16:55:59 -0800

    Fix Docker build

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,8 @@ module.exports = {
   compilers: {
     solc: {
       version: '0.5.16',
-      docker: !process.env.COVERALLS_REPO_TOKEN, // use docker except in CircleCI
+      docker: process.env.DOCKER_COMPILER !== undefined
+        ? process.env.DOCKER_COMPILER === 'true' : !process.env.COVERALLS_REPO_TOKEN, // Don't use Docker on CircleCI
       parser: 'solcjs',
       settings: {
         optimizer: {
@@ -27,6 +28,12 @@ module.exports = {
       port: 8555,
       gasPrice: 1,
       network_id: '1002',
+    },
+    docker: {
+      host: 'localhost',
+      network_id: '1313',
+      port: 8545,
+      gasPrice: 1,
     },
   },
   plugins: ['solidity-coverage'],
