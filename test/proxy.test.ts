836bcdf69f07299769c13267054594e58ff261f0
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-01-31 16:02:38 -0800

    Respond to CR

--- test/proxy.test.ts
+++ test/proxy.test.ts
@@ -24,24 +24,24 @@ perpetualDescribe('Proxy', initializeWithTestContracts, (ctx: ITestContext) => {
 
   describe('changeAdmin()', () => {
     it('starts on account 0', async () => {
-      await expectAdmin(ctx, ctx.accounts[0]);
+      await expectAdmin(ctx.accounts[0]);
     });
 
     it('succeeds', async () => {
       await ctx.perpetual.proxy.changeAdmin(ctx.accounts[1], { from: ctx.accounts[0] });
-      await expectAdmin(ctx, ctx.accounts[1]);
+      await expectAdmin(ctx.accounts[1]);
     });
 
     it('succeeds twice', async () => {
       await ctx.perpetual.proxy.changeAdmin(ctx.accounts[1], { from: ctx.accounts[0] });
-      await expectAdmin(ctx, ctx.accounts[1]);
+      await expectAdmin(ctx.accounts[1]);
       await ctx.perpetual.proxy.changeAdmin(ctx.accounts[2], { from: ctx.accounts[1] });
-      await expectAdmin(ctx, ctx.accounts[2]);
+      await expectAdmin(ctx.accounts[2]);
     });
 
     it('fails to do a second time', async () => {
       await ctx.perpetual.proxy.changeAdmin(ctx.accounts[1], { from: ctx.accounts[0] });
-      await expectAdmin(ctx, ctx.accounts[1]);
+      await expectAdmin(ctx.accounts[1]);
       await expectThrow(
         ctx.perpetual.proxy.changeAdmin(ctx.accounts[2], { from: ctx.accounts[0] }),
       );
@@ -55,9 +55,9 @@ perpetualDescribe('Proxy', initializeWithTestContracts, (ctx: ITestContext) => {
   describe('upgradeToAndCall()', () => {
     // TODO
   });
-});
 
-async function expectAdmin(ctx: ITestContext, address: address) {
-  const currentAdmin = await ctx.perpetual.proxy.admin({ from: address });
-  expect(currentAdmin).to.equal(address);
-}
+  async function expectAdmin(address: address) {
+    const currentAdmin = await ctx.perpetual.proxy.admin({ from: address });
+    expect(currentAdmin).to.equal(address);
+  }
+});
