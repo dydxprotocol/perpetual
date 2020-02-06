b5fe4b9145b41f3ea1d9c36d8bfa715085cf1df5
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-02-06 10:59:28 -0800

    Clean up Perpetual.ts

--- test/helpers/Perpetual.ts
+++ test/helpers/Perpetual.ts
@@ -1,16 +1,14 @@
 require('dotenv-flow').config();
 
-import { address, Provider } from '../../src/lib/types';
+import { address } from '../../src/lib/types';
 import { Perpetual } from '../../src/Perpetual';
-import _provider from './Provider';
-
-const provider = _provider as unknown as Provider;
+import provider from './Provider';
 
 let defaultAccountSet = false;
 let accounts: address[];
 
 export const perpetual = new Perpetual(
-  provider as any,
+  provider,
   Number(process.env.NETWORK_ID),
 );
 
