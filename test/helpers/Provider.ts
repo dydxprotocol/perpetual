80432659f3860d11c00af18effac6a9a65c14c9e
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-02-25 12:47:02 -0800

    wip

--- test/helpers/Provider.ts
+++ test/helpers/Provider.ts
@@ -1,5 +1,6 @@
 require('dotenv-flow').config();
 
+import { ProfilerSubprovider } from '@0x/sol-profiler';
 import { RevertTraceSubprovider, TruffleArtifactAdapter } from '@0x/sol-trace';
 import Web3 from 'web3';
 import ProviderEngine from 'web3-provider-engine';
@@ -7,19 +8,32 @@ import RpcSubprovider from 'web3-provider-engine/subproviders/rpc';
 
 import { Provider } from '../../src/lib/types';
 
+const enableProfiler = process.env.ENABLE_SOL_PROFILER === 'true';
+const enableTrace = process.env.ENABLE_SOL_TRACE === 'true';
+
+/**
+ * Creates a provider engine for use with 0x's Solidity dev tools.
+ */
 function providerEngine() {
   const artifactAdapter = new TruffleArtifactAdapter(
     '.', // project root
     '0.5.16', // solc version
   );
 
-  const revertTraceSubprovider = new RevertTraceSubprovider(
-    artifactAdapter,
-    '0x0000000000000000000000000000000000000000', // default from address
-  );
+  const defaultFromAddress = '0x0000000000000000000000000000000000000000';
 
   const providerEngine = new ProviderEngine();
-  providerEngine.addProvider(revertTraceSubprovider);
+  if (enableProfiler) {
+    const profilerSubprovider = new ProfilerSubprovider(artifactAdapter, defaultFromAddress);
+    providerEngine.addProvider(profilerSubprovider);
+    console.log('okaaaaaaay');
+  } else {
+    console.log('oooooops');
+  }
+  if (enableTrace) {
+    const revertTraceSubprovider = new RevertTraceSubprovider(artifactAdapter, defaultFromAddress);
+    providerEngine.addProvider(revertTraceSubprovider);
+  }
   providerEngine.addProvider(new RpcSubprovider({ rpcUrl: process.env.RPC_NODE_URI }));
   providerEngine.send = providerEngine.sendAsync as any;
 
@@ -31,11 +45,11 @@ function providerEngine() {
   return providerEngine;
 }
 
-function httpProvider() {
-  return new Web3.providers.HttpProvider(process.env.RPC_NODE_URI);
+let provider: Provider;
+if (enableProfiler || enableTrace) {
+  provider = providerEngine() as unknown as Provider;
+} else {
+  provider = new Web3.providers.HttpProvider(process.env.RPC_NODE_URI);
 }
-
-const provider: Provider = process.env.ENABLE_SOL_TRACE === 'true' ?
-  (providerEngine() as unknown as Provider) : httpProvider();
 
 export default provider;
