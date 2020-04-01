7155ed1e53be52fc07013f9d3dd9a7a7cb1db902
Brendan Chou <brendanchou@msn.com>
2020-03-31 18:42:06 -0700

    v.0.0.4: Deploy to Kovan and fix npm package publish (#124)
    * deploy to kovan and fix bug
    
    * 0.0.4
    
    * publish json
    
    * test
    
    * test 2

--- scripts/Artifacts.ts
+++ scripts/Artifacts.ts
@@ -5,7 +5,7 @@
 import { default as PerpetualProxy } from '../build/contracts/PerpetualProxy.json';
 import { default as PerpetualV1 } from '../build/contracts/PerpetualV1.json';
 import { default as P1FundingOracle } from '../build/contracts/P1FundingOracle.json';
-import { default as P1MakerOracle } from '../build/contracts/P1Orders.json';
+import { default as P1MakerOracle } from '../build/contracts/P1MakerOracle.json';
 import { default as P1Orders } from '../build/contracts/P1Orders.json';
 import { default as P1Deleveraging } from '../build/contracts/P1Deleveraging.json';
 import { default as P1Liquidation } from '../build/contracts/P1Liquidation.json';
