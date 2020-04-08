d1a5a2ba94f210b3d0b4f25507a4212e5c165ca1
Brendan Chou <brendanchou@msn.com>
2020-04-08 15:08:25 -0700

    fix lint

--- contracts/protocol/lib/ReentrancyGuard.sol
+++ contracts/protocol/lib/ReentrancyGuard.sol
@@ -21,16 +21,10 @@ pragma experimental ABIEncoderV2;
 
 
 /**
- * @dev Contract module that helps prevent reentrant calls to a function.
+ * @title ReentrancyGuard
+ * @author dYdX
  *
- * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
- * available, which can be applied to functions to make sure there are no nested
- * (reentrant) calls to them.
- *
- * Note that because there is a single `nonReentrant` guard, functions marked as
- * `nonReentrant` may not call one another. This can be worked around by making
- * those functions `private`, and then adding `external` `nonReentrant` entry
- * points to them.
+ * Updated ReentrancyGuard library designed to be used with Proxy Contracts.
  */
 contract ReentrancyGuard {
     uint256 private constant _NOT_ENTERED = 1;
@@ -39,33 +33,13 @@ contract ReentrancyGuard {
     uint256 private _status;
 
     constructor () internal {
-        // Storing an initial non-zero value makes deployment a bit more
-        // expensive, but in exchange the refund on every call to nonReentrant
-        // will be lower in amount. Since refunds are capped to a percetange of
-        // the total transaction's gas, it is best to keep them low in cases
-        // like this one, to increase the likelihood of the full refund coming
-        // into effect.
         _status = _NOT_ENTERED;
     }
 
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
-     * `private` function that does the actual work.
-     */
     modifier nonReentrant() {
-        // On the first call to nonReentrant, _notEntered will be true
         require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        // Any calls to nonReentrant after this point will fail
         _status = _ENTERED;
-
         _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
         _status = _NOT_ENTERED;
     }
 }
